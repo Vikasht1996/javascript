6b3d03979f62dc958d93cf8179e4186a153f006c
Vikas <vthammannaOSTB1@gmail.com>
2019-05-20 16:31:35 +0530

    Task1 commit

---
+++ Task1/javascript/home1.js
@@ -0,0 +1,10 @@
+// var a = 'welcome to "javascript" ';
+
+// console.log(a);
+
+{
+	let a="10";
+	 a="11";
+	document.write(a);
+}
+// document.write(b);
---
+++ Task1/javascript/home2.js
@@ -0,0 +1,4 @@
+var a = "Hello world"; b = "welcome"; c = "javascript"; 
+var d="Hi good afternoon";
+var e ="had ur lunch"; 
+document.write(a+ "<br>" +b+"<br>"+c+"<br>"+d+" "+e);
